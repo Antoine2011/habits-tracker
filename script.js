49b73f05d7141c3f97bebfacb3c214bac459ce41
Antoine2011 <zhengantoine2@gmail.com>
2026-04-25 17:42:12 +0200

    Update script.js

--- script.js
+++ script.js
@@ -1,12 +1,4 @@
 // ================= FIREBASE CONFIG =================
-// Import the functions you need from the SDKs you need
-import { initializeApp } from "firebase/app";
-import { getAnalytics } from "firebase/analytics";
-// TODO: Add SDKs for Firebase products that you want to use
-// https://firebase.google.com/docs/web/setup#available-libraries
-
-// Your web app's Firebase configuration
-// For Firebase JS SDK v7.20.0 and later, measurementId is optional
 const firebaseConfig = {
   apiKey: "AIzaSyDr39wbDbC0pE5huN_izcfmr5f9ODA4qD0",
   authDomain: "habits-tracker-4ee66.firebaseapp.com",
